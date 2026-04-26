af6ff6bbecacae6f23317b11904de21f43ce4cb4
PerculiarOlivia <nomhletshehla20@gmail.com>
2026-04-26 17:51:22 +0200

    uploaded channel stored procedure

---
+++ creating_stored_procedures/4. sp_channel.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE sp_create_dim_channel
+AS
+BEGIN
+--dim channel
+DROP TABLE [ComputerStd].[dbo].[dim_channel]
+CREATE TABLE [ComputerStd].[dbo].[dim_channel](
+[ChannelID]  INT IDENTITY(1,1) PRIMARY KEY,
+	[Channel] [nvarchar](50) NOT NULL
+)
+INSERT INTO [ComputerStd].[dbo].[dim_channel] (Channel)
+SELECT DISTINCT Channel
+FROM [ComputerStd].[dbo].[raw_pc_data]
+END;
