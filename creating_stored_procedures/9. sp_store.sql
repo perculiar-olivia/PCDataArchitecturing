dcaf86a4ff101f430d3a9651a1036e33e62951ab
PerculiarOlivia <nomhletshehla20@gmail.com>
2026-04-26 18:00:01 +0200

    uploaded store stored procedure

---
+++ creating_stored_procedures/9. sp_store.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE sp_create_dim_store
+AS
+BEGIN
+--dim store
+DROP TABLE [ComputerStd].[dbo].[dim_store]
+CREATE TABLE [ComputerStd].[dbo].[dim_store](
+[StoreID] INT IDENTITY(1,1) PRIMARY KEY,
+    [Shop_Name] [nvarchar](50) NOT NULL,
+	[Shop_Age] [nvarchar](50) NOT NULL
+) 
+INSERT INTO [ComputerStd].[dbo].[dim_store] (Shop_Name,Shop_Age)
+SELECT DISTINCT Shop_Name,Shop_Age
+FROM [ComputerStd].[dbo].[raw_pc_data]
+END;
